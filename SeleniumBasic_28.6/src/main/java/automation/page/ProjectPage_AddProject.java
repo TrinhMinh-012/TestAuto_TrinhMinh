2b33408a4a8193febbc14d53554e99b8c897ae74
MINH Trịnh <trinhducminh178@gmail.com>
2023-09-20 18:27:11 +0700

    20/09

--- SeleniumBasic_28.6/src/main/java/automation/page/ProjectPage_AddProject.java
+++ SeleniumBasic_28.6/src/main/java/automation/page/ProjectPage_AddProject.java
@@ -5,44 +5,60 @@ import static org.testng.Assert.assertTrue;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Pause;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import automation.common.CommonBase;
 import automation.testsuite.AddProject_Test;
 
-public class ProjectPage_AddProject {
-	@FindBy(xpath= "//span[text()='Projects' and @class='menu-text ']")
-	private WebElement buttonProjects ;
-	@FindBy(xpath= "//a[text()=' Add project' and @title='Add project' ]")
-	private WebElement buttonAddProjects ;
-	
-	@FindBy(id= "title")
-	private WebElement txtTitle;
-	
-//	@FindBy(xpath= "//button[@type='submit' and text()=' Save']")
-//	private WebElement buttonSave ;
+public class ProjectPage_AddProject  extends CommonBase{
+
+
 	
 	public void  AddProjectPage(WebDriver driver) {
 		this.driver = driver;
-	}
+	};
 	
-	@FindBy(xpath= "//button[@type='submit' and text()=' Save']")
-	private WebElement buttonSave ;
+	
+	
+	private By buttonProjects = By.xpath("//span[text()='Projects' and @class='menu-text ']");
+	private By buttonAddProjects = By.xpath("//a[text()=' Add project' and @title='Add project' ]");
+	private By txtTitle = By.id("title");
+	private By buttonSave = By.xpath("//button[@type='submit' and text()=' Save']");
+	private By DropClient = By.xpath("//span[@id='select2-chosen-9']");
+	private By start_Date = By.xpath("//input[@name='start_date']");
+	private By client = By.xpath("//span[@id='select2-chosen-109']");
+	private By label = By.xpath("//input[@id='s2id_autogen8']");
+	
+	
 	private WebDriver driver ;
-
+	
+	
 	public ProjectPage_AddProject (WebDriver driver) {	
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	public void AddProject( String title) {
-		buttonProjects.click();
-		buttonAddProjects.click();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		txtTitle.sendKeys(title);
-		buttonSave.click();
+	public void AddProject( String title, String start_date, String label1,String label2  ) {
+		click(buttonProjects);
+		click(buttonAddProjects);
+		type(txtTitle, title);
+//		click(DropClient);
+//		pause(3000);
+//		type(DropClient,"Internal Project");
+//		driver.findElement(DropClient).sendKeys(Keys.TAB);
+		type(client,"Hauck Ltd");
+		driver.findElement(client).sendKeys(Keys.TAB);
+		type(start_Date, start_date);
+		type(label, label1);
+		driver.findElement(label).sendKeys(Keys.TAB);
+		type(label, label2);
+		driver.findElement(label).sendKeys(Keys.TAB);
+		click(buttonSave);
+		
 	}
 	
 }
--- SeleniumBasic_28.6/src/main/java/automation/testsuite/AddProject_Test.java
+++ SeleniumBasic_28.6/src/main/java/automation/testsuite/AddProject_Test.java
@@ -21,24 +21,24 @@ public class AddProject_Test extends CommonBase {
 	@BeforeTest
 	public void Add_Project() {
 		driver = initChromeDriver(AccoutConstant.WebUrl);
+		LoginPageFactory factory = new LoginPageFactory(driver);
+		factory.LoginFunctionFactory("admin@demo.com", "riseDemo");
 	}
 	@Test
 	public void loginSuccessfully() {
-		LoginPageFactory factory = new LoginPageFactory(driver);
-		factory.LoginFunctionFactory("admin@demo.com", "riseDemo");
-		
-		DashboardPage dashboardPage = new DashboardPage();
-		pause(4000);;
-		WebElement expected = driver.findElement(dashboardPage.dashboardText);
-		assertTrue(expected.isDisplayed());
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-		ProjectPage_AddProject AddProjects = new ProjectPage_AddProject(driver);
-		AddProjects.AddProject( "svnd");
+//		LoginPageFactory factory = new LoginPageFactory(driver);
+//		factory.LoginFunctionFactory("admin@demo.com", "riseDemo");
+//		
+//		DashboardPage dashboardPage = new DashboardPage();
+//		pause(4000);;
+//		WebElement expected = driver.findElement(dashboardPage.dashboardText);
+//		assertTrue(expected.isDisplayed());
+////		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
 		
 		
+		ProjectPage_AddProject addProjects = new ProjectPage_AddProject(driver);
+		addProjects.AddProject( "asc", "10-10-2023", "On track","Perfect");
 	}
-
-	
 	
 	@AfterClass
 	public void closeDriver() {
